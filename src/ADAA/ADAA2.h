f61b46f208e2edca94a8979d8ad1899d31118f98
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-11-29 21:58:53 +0000

    Fix equation issue in ADAA2

--- src/ADAA/ADAA2.h
+++ src/ADAA/ADAA2.h
@@ -12,6 +12,7 @@ public:
     {
         ADAA1::prepare();
         x2 = 0.0;
+        ad2_x0 = 0.0;
         ad2_x1 = 0.0;
         d2 = 0.0;
     }
@@ -29,6 +30,7 @@ public:
         d2 = d1;
         x2 = x1;
         x1 = x;
+        ad2_x1 = ad2_x0;
         
         return y;
     }
@@ -37,6 +39,7 @@ protected:
     virtual inline double nlFunc_AD2 (double x) const noexcept { return func_AD2 (x); }
 
     double x2 = 0.0;
+    double ad2_x0 = 0.0;
     double ad2_x1 = 0.0;
     double d2 = 0.0;
 
@@ -44,26 +47,25 @@ private:
     inline double calcD1 (double x0) noexcept
     {
         bool illCondition = std::abs (x0 - x1) < ADAAConst::TOL;
-        double ad2_x0 = nlFunc_AD2 (x0);
+        ad2_x0 = nlFunc_AD2 (x0);
 
         double y = illCondition ?
             nlFunc_AD1 (0.5 * (x0 + x1)) :
             (ad2_x0 - ad2_x1) / (x0 - x1);
 
-        ad2_x1 = ad2_x0;
         return y;
     }
 
     inline double fallback (double x) noexcept
     {
         double xBar = 0.5 * (x + x2);
-        double delta = xBar - x;
+        double delta = xBar - x1;
 
         bool illCondition = std::abs (delta) < ADAAConst::TOL;
 
         return illCondition ?
-            nlFunc (0.5 * (xBar + x)) :
-            (2.0 / delta) * (nlFunc_AD1 (xBar) + (nlFunc_AD2 (x) - nlFunc_AD2 (xBar)) / delta);
+            nlFunc (0.5 * (xBar + x1)) :
+            (2.0 / delta) * (nlFunc_AD1 (xBar) + (ad2_x1 - nlFunc_AD2 (xBar)) / delta);
     }
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ADAA2)
